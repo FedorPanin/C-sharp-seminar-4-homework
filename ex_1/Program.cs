222bd3af2f928ec3b6bae4b39b53adb9d540a1bf
Mess <alla-fedia@mail.ru>
2023-03-16 18:58:06 +0300

    1

---
+++ ex_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите степень");
+int x = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+res = (int)Math.Pow(n, x);
+Console.WriteLine(res);
---
+++ ex_3/Program.cs
@@ -0,0 +1,6 @@
+﻿int[]arr = new int [8];
+for (int i =0; i < 8; i++)
+{
+	arr[i] = new Random().Next(1,9);
+	Console.Write(arr[i] + " ");
+}
